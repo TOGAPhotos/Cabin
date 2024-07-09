bf866b7e7ba3c030f0cd08d4d857255195c57126
794TD <davidyan003@gmail.com>
2024-07-09 15:21:48 +0800

    调整图片浏览页面信息展示样式；

---
+++ src/components/InfoLabel.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const props = defineProps({
+  label:String,
+  value: String,
+})
+</script>
+
+<template>
+<div class="info-label">
+  <div class="label"><span style="background: #001A38; padding: 2px 9px">{{props.label}}</span></div>
+  <div class="value">{{props.value}}</div>
+</div>
+</template>
+
+<style scoped>
+.info-label{
+  margin-bottom: 0.6em;
+
+}
+.value{
+  min-height: 1em;
+}
+.label{
+  font-size: 0.8em;
+  font-weight: bolder;
+  margin-bottom: 0.4em;
+  color: white;
+}
+
+</style>
