50e959b362a5fe65a7999488edf782d4f940242d
794TD <davidyan003@gmail.com>
2024-07-09 15:23:25 +0800

    重构主页；

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,12 +7,14 @@ import Thumbnail from "@/components/Thumbnail.vue";
 import {ElNotification} from "element-plus";
 import StatisticInfoBox from "@/components/StatisticInfoBox.vue";
 import HeadPhoto from "@/components/HeadPhoto.vue";
+import Activity from "@/components/Activity.vue";
+import type {PhotoInfo} from "@/utils/type/photo";
 
 const photoList = ref<PhotoInfo[]>([]);
 
 
-const photoListReq = new serverRequest('GET', "/photos/full?lastId=-1");
-photoListReq.success = () => photoList.value = photoListReq.getData('data')
+const photoListReq = new serverRequest('GET', "/photos?lastId=-1");
+photoListReq.success = () => photoList.value = photoListReq.getData()
 
 const notamReq = new serverRequest('GET', "/notam");
 notamReq.success = () => {
@@ -39,8 +41,8 @@ const bottomLoad = new BottomLoader(async () => {
     return;
   }
   const lastImgId = photoList.value[photoList.value.length - 1]['id'];
-  const appendListReq = new serverRequest('GET', `/photos/full?lastId=${lastImgId}`,);
-  appendListReq.success = () => photoList.value = photoList.value?.concat(appendListReq.getData('data'))
+  const appendListReq = new serverRequest('GET', `/photos?lastId=${lastImgId}`,);
+  appendListReq.success = () => photoList.value = photoList.value?.concat(appendListReq.getData())
   await appendListReq.send();
 })
 
@@ -55,9 +57,16 @@ onMounted(() => {
 </script>
 <template>
   <div>
-    <HeadPhoto/>
-    <!--    <Activity/>-->
-    <StatisticInfoBox/>
+    <div class="top">
+      <HeadPhoto class="head-photo"/>
+      <div class="top-side">
+        <Activity/>
+        <StatisticInfoBox/>
+      </div>
+
+
+    </div>
+
 
     <div class="content-box">
       <div class="content-box-title">
@@ -66,10 +75,10 @@ onMounted(() => {
       <div class="content-box-main">
         <Thumbnail v-for="photo in photoList" :key="photo.id"
                    :id="photo.id"
-                   :reg="photo.reg"
+                   :reg="photo.ac_reg"
                    :airline="photo.airline"
                    :username="photo.username"
-                   :airType="photo.airtype"
+                   :airType="photo.ac_type"
         />
       </div>
     </div>
@@ -77,5 +86,29 @@ onMounted(() => {
 </template>
 
 <style scoped>
+@media only screen and (max-width: 701px) {
+  .head-photo{
+    width: 100%;
+  }
+  .top-side{
+    width: 100%;
+  }
+}
 
+@media only screen and (min-width: 701px) {
+  .head-photo{
+    width: 60%;
+  }
+  .top-side{
+    width: 30%;
+  }
+}
+
+.top{
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+
+}
 </style>
