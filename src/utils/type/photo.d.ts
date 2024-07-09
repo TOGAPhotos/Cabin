071c59966f8f42e7a9cf5afcc7f6dcc4d1bda57e
794TD <davidyan003@gmail.com>
2024-07-09 15:10:53 +0800

    调整图片数据类型

--- src/utils/type/photo.d.ts
+++ src/utils/type/photo.d.ts
@@ -1,7 +1,27 @@
+import type {AirportData} from "@/utils/type/airport";
+
 declare type PhotoInfo = {
     airline:string,
-    airtype:string,
+    ac_type:string,
     id:number,
-    reg:string,
+    ac_reg:string,
     username:string,
+}
+
+interface FullPhotoInfo extends PhotoInfo {
+    airport_id: number,
+    airport_cn:string,
+    airport_iata_code: string,
+    airport_icao_code: string,
+
+    allow_socialmedia: number,
+
+    // manufacturer_cn: string,
+    // manufacturer_en: string,
+    ac_msn: string,
+
+    photo_time: string,
+    user_remark: string,
+
+    upload_user_id: number,
 }
