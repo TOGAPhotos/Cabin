b6a0d32c757d92e26ec3f2848c591a4b54ea5be5
794TD <davidyan003@gmail.com>
2024-07-14 22:57:58 +0800

    为infoLabel添加跳转能力

--- src/components/InfoLabel.vue
+++ src/components/InfoLabel.vue
@@ -2,13 +2,15 @@
 const props = defineProps({
   label:String,
   value: String,
+  link:String
 })
 </script>
 
 <template>
 <div class="info-label">
   <div class="label"><span style="background: #001A38; padding: 2px 9px">{{props.label}}</span></div>
-  <div class="value">{{props.value}}</div>
+  <div class="value" v-if="props.link"><a class="link" :href="props.link">{{props.value}}</a></div>
+  <div class="value" v-else><span>{{props.value}}</span></div>
 </div>
 </template>
 
@@ -25,6 +27,14 @@ const props = defineProps({
   font-weight: bolder;
   margin-bottom: 0.4em;
   color: white;
+}
+.value a{
+  color: black;
+}
+.value a.link:hover{
+  padding: 0 3px;
+  color: white;
+  background: #001A38;
 }
 
 </style>
