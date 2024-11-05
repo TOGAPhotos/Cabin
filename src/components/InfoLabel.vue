3a5afa5cbb6d7d50a0748e524a8bd096c046613b
794TD <davidyan003@gmail.com>
2024-11-05 23:13:41 +0800

    更新InfoLabel组件，允许通过v-html渲染链接值

--- src/components/InfoLabel.vue
+++ src/components/InfoLabel.vue
@@ -9,7 +9,7 @@ const props = defineProps({
 <template>
 <div class="info-label">
   <div class="label"><span style="background: #001A38; padding: 2px 9px">{{props.label}}</span></div>
-  <div class="value" v-if="props.link"><a class="link" :href="props.link">{{props.value}}</a></div>
+  <div class="value" v-if="props.link"><a class="link" :href="props.link" v-html="props.value"></a></div>
   <div class="value" v-else><span>{{props.value}}</span></div>
 </div>
 </template>
