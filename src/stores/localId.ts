6f3875f177adc85500fe6d3af29df0a67fed1086
794TD <davidyan003@gmail.com>
2024-11-05 23:17:03 +0800

    新增localId存储，优化请求头设置逻辑以支持使用t_Id作为客户端标识符追踪请求

---
+++ src/stores/localId.ts
@@ -0,0 +1,30 @@
+import { defineStore } from 'pinia'
+interface localId {
+    id:string|null,
+    time:number|null,
+}
+export const useLocalIdStore = defineStore('local_id',{
+    state: ():localId => ({
+        id:null,
+        time:null,
+    }),
+    getters:{
+        isAvail: state => (state.id !== null) && (state.time && Date.now() - state.time < 1000*60*60*24*15),
+    },
+    actions: {
+        setId(){
+            if( this.time && Date.now() - this.time < 1000*60*60*24*15){
+                return;
+            }
+            this.id = crypto.randomUUID();
+            this.time = Date.now();
+        },
+        clearId(){
+            this.id = null;
+            this.time = null;
+        }
+    },
+    persist: true
+})
+
+export default useLocalIdStore
