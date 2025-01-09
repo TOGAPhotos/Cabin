410a225db4ebd3d995e418e9517c09694779dc15
794TD <davidyan003@gmail.com>
2025-01-09 12:29:35 +0800

    Newly added about page and 404 page components

---
+++ src/views/other/Page404.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-result
+      style="padding-top: 20vh"
+      icon="warning"
+      title="您访问的页面不存在"
+  >
+    <template #extra>
+      <router-link to="/">
+        <el-button type="primary" >返回主页</el-button>
+      </router-link>
+    </template>
+  </el-result>
+</template>
