9ffc7570a113b9b6171eb6df82649d89c945b6ef
794TD <davidyan003@gmail.com>
2025-03-01 16:46:58 +0800

    优化 NewPhotos 组件，更新数据请求逻辑，修复导入路径，调整类型定义

--- src/components/NewPhotos.vue
+++ src/components/NewPhotos.vue
@@ -1,23 +1,21 @@
 <script setup lang="ts">
-import {onMounted, ref, inject, type Ref} from "vue";
-import serverRequest from "@/utils/request";
-
-// import Thumbnail from "@/components/Thumbnail.vue";
+import {onMounted, ref } from "vue";
+import ServerRequest from "@/utils/request";
+import Thumbnail from "@/components/Thumbnail.vue";
 import PhotoBox from "@/components/photo/PhotoBox.vue";
 
-const screenWidth = inject("screenWidth") as Ref<number>;
+import type { AcceptPhoto } from "@/utils/type/photo";
+import Device from "@/utils/device";
 
-const photoList = ref<PhotoInfo[]>([]);
-const photoListReq = new serverRequest('GET', "/photos/full?lastId=-1");
-photoListReq.success = () => photoList.value = photoListReq.getData('data')
 
-onMounted(() => {
-    Promise.allSettled([
-      photoListReq.send(),
-    //   notamReq.send(),
-    ]).then(()=>{
-        console.log(photoList.value)
-    })
+
+const photoList = ref<AcceptPhoto[]>([]);
+const photoListReq = new ServerRequest('GET', "/photos");
+photoListReq.success = () => photoList.value = photoListReq.getData()
+
+onMounted(async () => {
+    await photoListReq.send();
+    console.log('photoList', photoList.value)
 })
 </script>
 
@@ -27,44 +25,25 @@ onMounted(() => {
         <h2>最新图片</h2>
       </div>
       <div class="content-box-main newphotos-photolist">
-        <div class="photobox" v-for="photo in photoList" :key="photo.id">
-            <PhotoBox :photoInfo="photo" :isMobile="screenWidth >= 1024 ? false : true" />
-        </div>
         
-        <!-- <Thumbnail v-for="photo in photoList" :key="photo.id"
+        <Thumbnail v-for="photo in photoList" :key="photo.id"
+                    class="gallery-photo"
                    :id="photo.id"
-                   :reg="photo.reg"
+                   :reg="photo.ac_reg"
                    :airline="photo.airline"
                    :username="photo.username"
-                   :airType="photo.airtype"
-        /> -->
+                   :airType="photo.ac_type"
+        />
       </div>
     </div>
 </template>
 
 <style scoped>
-.newphotos-box{
-    /* background-color: red; */
-}
 .newphotos-photolist{
-    /* background-color: pink; */
+    max-width: var(--max-width);
     padding: 2rem 0rem;
     display: flex;
-    justify-content: space-between;
     align-items: center;
     flex-wrap: wrap;
-    gap: 1rem;
-}
-.newphotos-photolist .photobox{
-    width: 100%;
-    max-width: 18.5rem;
-}
-@media only screen and (max-width: 1023px) {
-    .newphotos-photolist .photobox{
-        /* width: 100%; */
-        /* max-width: 25rem; */
-        flex: 1 1 45%;
-        min-width: 30%;
-    }
 }
 </style>
