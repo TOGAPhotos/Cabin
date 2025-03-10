a982909540a0b0d4ea192329fca41e2fdff5f732
794TD <davidyan003@gmail.com>
2025-03-10 17:24:32 +0800

    更新 CreateAirtype 组件，重构表单数据处理逻辑，使用模板引用管理表单状态，并优化提交验证逻辑

--- src/components/CreateAirtype.vue
+++ src/components/CreateAirtype.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import {computed, reactive, ref} from "vue";
+import {computed, reactive, ref, useTemplateRef} from "vue";
 import Device from "@/utils/device";
-import {ElMessage} from "element-plus";
+import {ElMessage, type FormInstance} from "element-plus";
 import ServerRequest from "@/utils/request";
 
 const show = defineModel({type:Boolean,default:false});
 const status = reactive({
   loading: false,
 })
-
-const form = reactive({
+const form = useTemplateRef<FormInstance>("formData");
+const formData = reactive({
   manufacturer_cn:"",
   manufacturer_en:"",
   type:"",
@@ -21,19 +21,20 @@ const setWidth = computed(()=> Device.isPhone() ? '100%' : 500)
 async function submit(){
   if(status.loading) return;
 
-  if(form.manufacturer_en === "" && form.manufacturer_cn === ""){
+  if(formData.manufacturer_en === "" && formData.manufacturer_cn === ""){
     ElMessage.error("制造商名称不能全部留空");
     return;
   }
-  if(form.type === "" || form.sub_type === ""){
+  if(formData.type === "" || formData.sub_type === ""){
     ElMessage.error("型号不能为空");
     return;
   }
 
   status.loading=true;
-  const req = new ServerRequest("POST","/airtype",form);
+  const req = new ServerRequest("POST","/airtype",formData);
   req.success = (msg) => {
     ElMessage.success(msg);
+    form.value!.resetFields();
     show.value = false;
   }
   req.error = (code,msg) => {
@@ -56,28 +57,29 @@ async function submit(){
     <el-divider/>
     <el-form
         label-position="top"
+        ref="form"
     >
       <el-form-item label="制造商中文名">
-        <el-input v-model="form.manufacturer_en" placeholder="例：空中客车"/>
+        <el-input v-model="formData.manufacturer_cn" placeholder="例：空中客车"/>
       </el-form-item>
       <el-form-item label="制造商英文名">
-        <el-input v-model="form.manufacturer_cn" placeholder="例：Airbus"/>
+        <el-input v-model="formData.manufacturer_en" placeholder="例：Airbus"/>
       </el-form-item>
       <el-form-item label="型号">
-        <el-input v-model="form.type" placeholder="例：A330"/>
+        <el-input v-model="formData.type" placeholder="例：A330"/>
       </el-form-item>
       <el-form-item label="具体子型号">
-        <el-input v-model="form.sub_type" placeholder="例：A330—343"/>
+        <el-input v-model="formData.sub_type" placeholder="例：A330—343"/>
       </el-form-item>
       <el-form-item label="ICAO代码">
-        <el-input v-model="form.icao_code" placeholder="例：A333"/>
+        <el-input v-model="formData.icao_code" placeholder="例：A333"/>
       </el-form-item>
     </el-form>
     <template #footer>
       <div class="dialog-footer">
         <el-button @click="show = false">取消</el-button>
         <el-button
-            type="primary" @click="submit()"
+            type="primary" @click="submit"
             v-loading="status.loading"
             :disabled="status.loading"
         >确认</el-button>
