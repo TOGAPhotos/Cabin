a8ed79053644213bcf4586a58ef08b8340b881f4
794TD <davidyan003@gmail.com>
2025-04-12 23:09:34 +0800

    feat(Maintenance.vue): add loading state and dev mode check

--- src/views/other/Maintenance.vue
+++ src/views/other/Maintenance.vue
@@ -2,7 +2,7 @@
 import ImgLoader from "@/components/ImgLoader.vue";
 import router from "@/router";
 import ServerRequest from "@/utils/request";
-import { onMounted, reactive } from "vue";
+import { onMounted, reactive,ref } from "vue";
 const info = reactive({
     startTime: '',
     endTime: '',
@@ -13,16 +13,19 @@ const info = reactive({
 const converTime = (time: string) => {
     return new Date(time + ' GMT+8');
 }
+const loading = ref(true);
 
 onMounted(async () => {
     const check = new ServerRequest('GET', '/website')
     check.success = () => {
-        // router.push('/')
+        if ( import.meta.env.MODE === 'development') return;
+        router.push('/')
     }
     const [maintenance, _] = await Promise.all([
         fetch('https://status.togaphotos.com/api/maintenance'),
         check.send(),
     ])
+    loading.value = false;
     if (maintenance.ok) {
         const data = await maintenance.json()
         const _s = converTime(data['start_time']);
@@ -44,15 +47,20 @@ onMounted(async () => {
         <div style="margin-bottom: 1em;">
             <ImgLoader src="/maintenance_fox.webp" alt="Maintenance Fox" protect />
         </div>
-        <div v-if="info._type === 'ERROR'">
-            <h2>目前没有维护计划</h2>
-            <p>出现这种情况，可能是您的网络不稳定，或者是网站正在经历突发故障。</p>
+        <div v-if="loading">
+            <h2>正在检查维护计划......</h2>
         </div>
         <div v-else>
-            <h2>正在进行{{ info.type }}维护</h2>
-            <h3>{{ info.remark }}</h3>
-            <p>开始时间：{{ info.startTime }}</p>
-            <p>结束时间：{{ info.endTime }}</p>
+            <div v-if="info._type === 'ERROR'">
+                <h2>目前没有维护计划</h2>
+                <p>出现这种情况，可能是您的网络不稳定，或者是网站正在经历突发故障。</p>
+            </div>
+            <div v-else>
+                <h2>正在进行{{ info.type }}维护</h2>
+                <h3>{{ info.remark }}</h3>
+                <p>开始时间：{{ info.startTime }}</p>
+                <p>结束时间：{{ info.endTime }}</p>
+            </div>
         </div>
     </div>
 </template>
