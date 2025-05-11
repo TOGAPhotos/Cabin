6ed152dd932c8520158dd141dc7acc5b29ef0035
Lyon817 <liangtianyi2002@outlook.com>
2025-05-11 22:52:07 +0800

    refactor(footer): to use tw

--- src/component/Footer.vue
+++ src/component/Footer.vue
@@ -3,9 +3,9 @@
     class="flex flex-col items-center gap-3 text-text-secondary pt-6 py-12"
   >
     <div class="flex gap-4 text-sm font-bold underline">
-      <a v-for="link in links" :key="link.href" :href="link.href">{{
-        link.label
-      }}</a>
+      <a v-for="link in links" :key="link.href" :href="link.href">
+        {{ link.label }}
+      </a>
     </div>
 
     <div class="flex flex-col items-center gap-3 text-xs font-semibold">
