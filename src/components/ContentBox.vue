73bc8b5edaf474e7e65598b2a05d0e37d5e0d749
Lyon817 <liangtianyi2002@outlook.com>
2025-06-15 22:54:41 +0800

    refactor(homepage): stats box

---
+++ src/components/ContentBox.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="p-6 flex gap-3 bg-white rounded-lg shadow-md text-shadow-text-primary transition-grow"
+  >
+    <div class="size-8">
+      <slot name="icon" />
+    </div>
+    <div class="flex flex-col gap-2">
+      <div class="text-lg font-bold">
+        <slot name="title" />
+      </div>
+      <div v-if="$slots.content">
+        <slot name="content" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
