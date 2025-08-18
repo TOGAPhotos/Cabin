e23396c43bb285b7a8dde871cd79de146c5ee68c
794TD <davidyan003@gmail.com>
2025-08-18 18:55:02 +0800

    feat: add permission meta to routes for user access control

--- src/utils/permission.ts
+++ src/utils/permission.ts
@@ -1,5 +1,21 @@
 export default class Permission {
-  static check(user: any, require: any) {
-    return user === require;
+  static readonly user = "USER";
+  static readonly media = "MEDIA";
+  static readonly database = "DATABASE";
+  static readonly screener1 = "SCREENER_1";
+  static readonly screener2 = "SCREENER_2";
+  static readonly admin = "ADMIN";
+  static readonly ROLE_LIST = [
+    Permission.user,
+    Permission.media,
+    Permission.database,
+    Permission.screener1,
+    Permission.screener2,
+    Permission.admin,
+  ];
+  static check(require: string, user: string) {
+    const userRoleIndex = Permission.ROLE_LIST.indexOf(user);
+    const requireRoleIndex = Permission.ROLE_LIST.indexOf(require);
+    return userRoleIndex >= requireRoleIndex;
   }
 }
