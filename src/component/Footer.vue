1619f8097d9799ce54b1d12460a778cd7fe27e57
794TD <davidyan003@gmail.com>
2025-11-14 01:30:20 -0500

    style: adjust footer padding and update text styling

--- src/component/Footer.vue
+++ src/component/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer
-    class="flex flex-col items-center gap-3 text-text-secondary pt-6 py-12"
+    class="flex flex-col items-center gap-3 text-text-secondary pt-6 py-6"
   >
     <div class="flex gap-4 text-sm font-bold underline">
       <a v-for="link in links" :key="link.href" :href="link.href">
@@ -8,7 +8,7 @@
       </a>
     </div>
 
-    <div class="flex flex-col items-center gap-3 text-xs font-semibold">
+    <div class="flex flex-col items-center text-xs font-extrabold">
       <div>Maintenance by TOGAPhotos Team</div>
       <div>Copyright © 2023-Present TOGAPhotos All Rights Reserved.</div>
     </div>
