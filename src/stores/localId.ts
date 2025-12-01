49c893da37a6f9847002aeb7d498a5f15729b3ab
794TD <davidyan003@gmail.com>
2025-11-30 21:29:33 -0500

    fix: replace hardcoded expiration time with constant for local ID

--- src/stores/localId.ts
+++ src/stores/localId.ts
@@ -4,6 +4,7 @@ interface localId {
   id: string | null;
   time: number | null;
 }
+const TRACE_ALIVE_TIME = 1000 * 60 * 60 * 12; // 12 hours
 
 export const useLocalIdStore = defineStore("local_id", {
   state: (): localId => ({
@@ -14,11 +15,11 @@ export const useLocalIdStore = defineStore("local_id", {
     isAvail: (state) =>
       state.id !== null &&
       state.time &&
-      Date.now() - state.time < 1000 * 60 * 60 * 24 * 15,
+      Date.now() - state.time < TRACE_ALIVE_TIME,
   },
   actions: {
     setId() {
-      if (this.time && Date.now() - this.time < 1000 * 60 * 60 * 24 * 15) {
+      if (this.time && Date.now() - this.time < TRACE_ALIVE_TIME) {
         return;
       }
       this.id = crypto.randomUUID();
