01c638d39705cf06b1e6ad42341536655bee1e0e
794TD <68456534+794TD@users.noreply.github.com>
2025-12-24 14:54:13 -0500

    Summary 2025 (#60)
    * chore: update VITE_API_BASE_URL and add stats.html to .gitignore
    
    * feat: enhance MyselfView with responsive design and new summary feature
    
    * fix: update summary2025 function to open summary page

--- src/views/user/MyselfView.vue
+++ src/views/user/MyselfView.vue
@@ -3,7 +3,9 @@ import AccountSetting from "@/component/AccountSetting.vue";
 import PhotoCard from "@/components/PhotoCard.vue";
 import router from "@/router";
 import useUserInfoStore from "@/stores/userInfo";
-import { ThumbnailUrl } from "@/utils/photo-url";
+import Device from "@/utils/device";
+import Permission from "@/utils/permission";
+import { PhotoUrl, ThumbnailUrl } from "@/utils/photo-url";
 import ServerRequest from "@/utils/request";
 import type { AirportData } from "@/utils/type/airport";
 import type { AcceptPhoto } from "@/utils/type/photo";
@@ -26,7 +28,7 @@ onMounted(async () => {
   await userInfoReq.send();
   if (userInfo.value?.cover_photo_id) {
     headerElm.value!.style.background = `linear-gradient(rgba(247, 248, 250, 0) 0%, rgba(247, 248, 250, 1) 100%),
-      url("${ThumbnailUrl(userInfo.value.cover_photo_id)}") no-repeat center/cover`;
+      url("${Device.isPhone() ? ThumbnailUrl(userInfo.value.cover_photo_id) : PhotoUrl(userInfo.value.cover_photo_id)}") no-repeat center/cover`;
   }
   if (userInfo.value?.airport_id) {
     const airportInfoReq = new ServerRequest(
@@ -51,6 +53,31 @@ const logout = async () => {
   await router.push("/");
   router.go(0);
 };
+
+const summary2025 = () => {
+  const origin = window.location.origin;
+  const newWindow = window.open("https://summary" + origin, "_blank");
+  window.addEventListener(
+    "message",
+    (event) => {
+      if (
+        event.origin !== "http://localhost:5173" &&
+        event.origin !== "https://summary.togaphotos.com" &&
+        event.origin !== "https://summary.toga.photos"
+      ) {
+        return;
+      }
+      if (event.data.type === "requestUserId") {
+        console.log("Received request for user token");
+        event.source?.postMessage(
+          { token: user.token },
+          { targetOrigin: event.origin },
+        );
+      }
+    },
+    { once: true },
+  );
+};
 </script>
 
 <template>
@@ -62,40 +89,6 @@ const logout = async () => {
         <div id="home-base">
           <div class="airport">{{ airportText }}</div>
         </div>
-        <!-- <div class="badge-box">
-          <el-popover class="badge" :width="300">
-            <template #default>
-              <img class="badge-big" src="https://cdn.photo.tp.794td.cn/badge/Badge_IB.jpg" alt=""/>
-            </template>
-            <template #reference>
-              <img class="badge-small" src="https://cdn.photo.tp.794td.cn/badge/Badge_IB.jpg" alt=""/>
-            </template>
-          </el-popover>
-          <el-popover :width="300">
-            <template #default>
-              <img class="badge-big" src="https://cdn.photo.tp.794td.cn/badge/Badge_IB.jpg" alt=""/>
-            </template>
-            <template #reference>
-              <img class="badge-small" src="https://cdn.photo.tp.794td.cn/badge/Badge_IB.jpg" alt=""/>
-            </template>
-          </el-popover>
-          <el-popover :width="300">
-            <template #default>
-              <img class="badge-big" src="https://cdn.photo.tp.794td.cn/badge/Badge_IB.jpg" alt=""/>
-            </template>
-            <template #reference>
-              <img class="badge-small" src="https://cdn.photo.tp.794td.cn/badge/Badge_IB.jpg" alt=""/>
-            </template>
-          </el-popover>
-          <el-popover :width="300">
-            <template #default>
-              <img class="badge-big" src="https://cdn.photo.tp.794td.cn/badge/Badge_IB.jpg" alt=""/>
-            </template>
-            <template #reference>
-              <img class="badge-small" src="https://cdn.photo.tp.794td.cn/badge/Badge_IB.jpg" alt=""/>
-            </template>
-          </el-popover>
-        </div> -->
       </div>
       <div class="statistic-box">
         <el-statistic
@@ -112,26 +105,38 @@ const logout = async () => {
         </el-statistic>
       </div>
       <div class="action-box">
-        <div class="action-item">
+        <div class="action-card">
           <a href="/upload">上传图片</a>
           <div class="explain">从这里上传您的照片到TOGAPhotos</div>
+        </div>
+        <div class="action-card">
           <a href="/queue/upload">上传队列</a>
           <div class="explain">
             查看正在等待审核的图片，您可以利用TOGAPhotos提供的工具自行检查您上传的图片。
           </div>
+        </div>
+        <div class="action-card">
           <a href="/queue/reject">未过审队列</a>
           <div class="explain">
             查看您最近10张没有通过TOGAPhotos审核的图片，了解您的图片没能通过审核的原因
           </div>
+        </div>
+        <div class="action-card">
           <a @click="settingPanelVisible = true">账户设置</a>
           <div class="explain">更改账户信息，调整您对第三方的授权</div>
+        </div>
+        <div class="action-card" v-if="Permission.isStaff(user.permission)">
+          <a @click="summary2025">2025年度总结</a>
+          <div class="explain">看一看你和我们一起走过的2025年</div>
+        </div>
+        <div class="action-card">
           <a style="color: #ff4d4a" @click="logout()">退出登录</a>
           <div class="explain">退出当前账户，注销本地存储的凭据</div>
         </div>
       </div>
     </div>
     <div class="photo-box">
-      <div class="grid grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-4">
+      <div class="grid grid-cols-2 md:grid-cols-2 xl:grid-cols-4 gap-4">
         <PhotoCard v-for="photo in photoList" v-bind="photo" :key="photo.id" />
       </div>
     </div>
@@ -143,6 +148,8 @@ const logout = async () => {
 #myself-page {
   display: flex;
   flex-wrap: wrap;
+  max-width: 1280px;
+  margin: 0 auto;
 }
 
 .self-intro {
@@ -158,9 +165,7 @@ const logout = async () => {
   flex-direction: column;
   justify-content: center;
 }
-.action-item a {
-  font-weight: bolder;
-}
+
 @media only screen and (min-width: 701px) {
   .myself-page {
     padding: 0 15px;
@@ -171,15 +176,17 @@ const logout = async () => {
   }
 
   .self-intro {
-    width: 40%;
+    width: 100%;
   }
+
   .photo-box {
-    width: 60%;
+    width: 100%;
   }
 
   .photo-box {
     margin: 24px 0 0 0;
   }
+
   .thumbnail {
     width: calc(100% / 3 - 12px) !important;
   }
@@ -256,13 +263,43 @@ const logout = async () => {
 .statistic-item {
   margin: 0 12px;
 }
+
 .action-box {
   margin: 12px;
-  border: #b1b3b8 solid 1px;
-  font-size: 0.9rem;
-  padding: 0.5rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  gap: 12px;
 }
+
+@media only screen and (max-width: 701px) {
+  .action-box {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+.action-card {
+  border: 1px solid #e4e7ed;
+  border-radius: 4px;
+  padding: 12px;
+  background-color: #fff;
+  transition: all 0.3s;
+}
+
+.action-card:hover {
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+}
+
+.action-card a {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 8px;
+  font-size: 16px;
+  cursor: pointer;
+}
+
 .explain {
-  margin: 0 0 0.4em 0;
+  font-size: 12px;
+  color: #909399;
+  margin: 0;
 }
 </style>
